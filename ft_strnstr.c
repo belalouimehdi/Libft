d1536eb9ec1aa7944f5497f3caebdbf84dbf4acd
mbelalou <m_belaloui@yahoo.fr>
2018-01-01 21:26:56 +0100

    mb

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -16,22 +16,19 @@ char	*ft_strnstr(const char *ha, const char *ne, size_t n)
 {
 	size_t	pt1;
 	size_t	pt2;
-	char	*result;
 
 	pt1 = 0;
-	result = NULL;
 	if (!ft_strlen(ne))
 		return ((char *)ha);
 	while (ha[pt1] && (pt1 < n))
 	{
 		pt2 = 0;
-		while ((ha[pt1 + pt2] == ne[pt2]) && ((pt1 + pt2) < n) && ne[pt2])
-		{
+		while (ha[pt1 + pt2] && ne[pt2] && (ha[pt1 + pt2] == ne[pt2])
+ && ((pt1 + pt2) < n))
 			pt2++;
-		}
 		if (!ne[pt2])
 			return ((char *)&ha[pt1]);
 		pt1++;
 	}
-	return ((char *)result);
+	return (NULL);
 }
