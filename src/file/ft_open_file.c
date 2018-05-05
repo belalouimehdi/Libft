204ae2b48d4c78be5106368b57a10f0d00c9c5f0
Mehdi aissa BELALOUI <mbelalou@e3r3p8.42.fr>
2018-05-05 20:09:37 +0200

    04/05/18

--- src/file/ft_open_file.c
+++ src/file/ft_open_file.c
@@ -6,7 +6,7 @@
 /*   By: mbelalou <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/10 01:03:35 by mbelalou          #+#    #+#             */
-/*   Updated: 2018/05/03 10:18:23 by mbelalou         ###   ########.fr       */
+/*   Updated: 2018/05/05 20:09:20 by mbelalou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ int		ft_open_file(char *url_file)
 	int fd;
 
 	if (url_file == NULL)
-		return (0);
+		return (-1);
 	fd = open(url_file, O_RDONLY);
 	if (fd < 0)
-		return (0);
+		return (-1);
 	return (fd);
 }
